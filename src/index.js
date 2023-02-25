c9fbab2885b5643b5cd17d00ef59176b403e10ff
Andrii <110056592+andriimonastyrskyi@users.noreply.github.com>
2023-02-25 10:22:21 +0200

    task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,89 @@
-// import Notiflix from 'notiflix';
-// import simpleLightbox from 'simplelightbox';
+import './css/styles.css';
+import { fetchImages } from './js/api';
+import { renderGallery } from './js/render';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// import fetchPhotos from './js/api.js';
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.btn-load-more');
 
-// const form = document.getElementById('search-form');
-// const inputSearch = document.getElementById('input-form');
-// const btnSubmit = document.getElementById('submit-form');
-// const gallery = document.querySelector('gallery');
-// const btnLoad = document.getElementById('load-more-btn');
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
+
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+function onSearchForm(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0 });
+  page = 1;
+  query = e.currentTarget.searchQuery.value.trim();
+  gallery.innerHTML = '';
+  loadMoreBtn.classList.add('is-hidden');
+
+  if (query === '') {
+    alertNoEmptySearch();
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        alertNoImagesFound();
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        alertImagesFound(data);
+
+        if (data.totalHits > perPage) {
+          loadMoreBtn.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function onLoadMoreBtn() {
+  page += 1;
+  simpleLightBox.destroy();
+
+  fetchImages(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPages) {
+        loadMoreBtn.classList.add('is-hidden');
+        alertEndOfSearch();
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function alertImagesFound(data) {
+  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+}
+
+function alertNoEmptySearch() {
+  Notiflix.Notify.failure(
+    'The search string cannot be empty. Please specify your search query.'
+  );
+}
+
+function alertNoImagesFound() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function alertEndOfSearch() {
+  Notiflix.Notify.failure(
+    "We're sorry, but you've reached the end of search results."
+  );
+}
--- src/js/api.js
+++ src/js/api.js
@@ -1,12 +1,25 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const URL = 'https://pixabay.com/api/';
+// const URL = 'https://pixabay.com/api/';
+// const KEY = '33742553-8bb04a1a84c196e6fa85b3bbd';
+
+// export async function fetchPhotos(query, perPage) {
+//   const response = await axios.get(
+//     `${URL}?key=${KEY}&q=${search}&image_type=photo&safesearch=true&orientation=horizontal&per_page=40&page=${perPage}`
+//   );
+//   return response.data;
+
+// }
+
+import axios from 'axios';
+export { fetchImages };
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '33742553-8bb04a1a84c196e6fa85b3bbd';
 
-export async function fetchPhotos(search, currentPage) {
+async function fetchImages(query, page, perPage) {
   const response = await axios.get(
-    `${URL}?key=${KEY}&q=${search}&image_type=photo&safesearch=true&orientation=horizontal&per_page=40&page=${currentPage}`
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   );
-  return response.data;
-  //   console.log();
+  return response;
 }
